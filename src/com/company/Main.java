61b2b917119297d549fb57d2d47a907a4f6c10ae
shelemetev <43704293+shelemetev@users.noreply.github.com>
2021-01-28 15:16:54 +0300

    end

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,20 @@ public class Main {
 	int monthOfBirthday = 11;
 	int dayOfBirthday = 23;
 	int sum = yearOfBirthday+monthOfBirthday+dayOfBirthday;
+	System.out.println("Сумма моеих года, месяца и дня рождения: "+sum);
+	boolean monthBiggerDay=monthOfBirthday>dayOfBirthday;
+	System.out.println("Месяц моего дня рождения больше дня: "+monthBiggerDay);
+	char[] name = {'К','и','р','и','л','л'};
+    System.out.print("Моё имя: ");
+    int i =0;
+    while (i<6) {
+    	System.out.print(name[i]);
+    	i++;
+		}
+    System.out.println();
+    double myAge = 18;
+    double partYear = 2.0/12.0;
+	myAge=myAge+partYear;
+    System.out.println("Мне "+myAge+" лет");
     }
 }
